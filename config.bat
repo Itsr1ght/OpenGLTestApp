05bcfea94d5e0bf571c46c7e5c664cd367aed86c
bilal <bilaljafar360@gmail.com>
2022-05-23 13:07:03 +0530

    Window Bat command for configuration and build is added

---
+++ config.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM generate the library from network using conan
+conan install . --build missing
+
+REM To Generate Windows Solution
+call vendor\premake\premake5.exe vs2019
+
+REM call Solution File
+cd build
+
+REM open the visual studio msbuild script
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsMSBuildCmd.bat"
+
+REM build the solution 
+msbuild OpenGLTestApp.sln
